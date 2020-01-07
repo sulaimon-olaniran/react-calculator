56c8dba2e847779d4adb095e14590e78b1ab0475
Olaniran Sulaimon <suzzwayne@gmail.com>
2020-01-07 20:24:30 +0100

    Updates

---
+++ precache-manifest.3df824ef3a9d6dbee446aa40ea835ea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8d062dfebbd1d37e90f51132a4a93f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "64cbf62cdec3ba08a553",
+    "url": "/react-calculator/static/css/main.d01e8d57.chunk.css"
+  },
+  {
+    "revision": "086d43e6f220a79ad0d8",
+    "url": "/react-calculator/static/js/2.886d66b2.chunk.js"
+  },
+  {
+    "revision": "64cbf62cdec3ba08a553",
+    "url": "/react-calculator/static/js/main.9e8fd632.chunk.js"
+  },
+  {
+    "revision": "ceec9457fcb1e1f556c6",
+    "url": "/react-calculator/static/js/runtime~main.418f33d6.js"
+  }
+]);
